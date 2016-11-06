e67ea24a1b0d72bfc171f127817ad6ca261aac6f
miya0001 <miya@wpist.me>
2016-11-07 00:50:21 +0900

    add some scripts

---
+++ bin/run-tests.js
@@ -0,0 +1,10 @@
+const phantomjs = require( 'phantomjs-prebuilt' )
+const spawn = require( 'child_process' ).spawn
+phantomjs.run(
+  '--webdriver=4444',
+  '--ignore-ssl-errors=yes',
+  '--cookies-file=/tmp/webdriver_cookie.txt'
+).then( program => {
+  const behat = spawn( 'vendor/bin/behat', [], { stdio: "inherit" } )
+  behat.on( 'exit', code => { program.kill() } )
+} )
